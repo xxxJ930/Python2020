67a1f43c157233c365fc345382ff32bcce348587
Tedu <xxx@qq.com>
2020-06-20 12:01:36 +0800

    init add

---
+++ http_test.py
@@ -0,0 +1,21 @@
+from socket import *
+
+sock = socket()
+sock.bind(("0.0.0.0", 8888))
+sock.listen(3)
+
+c, addr = sock.accept()
+print("Connect from..", addr)
+data = c.recv(4096)
+print(data.decode())
+
+response = "HTTP/1.1 200 OK\r\n"
+response += "Content-Type:text/html\r\n"
+response += "\r\n"
+response += "FK ALL !!!"
+
+print(response)
+c.send(response.encode())
+
+c.close()
+sock.close()
